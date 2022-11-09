bb3a2fa562044e2e6be63ba74d14b8e9d830baad
Adamicina <jelza@stud.uniza.sk>
2022-11-09 11:23:39 +0100

    idk

---
+++ Minesweeper/Tlacitko.java
@@ -0,0 +1,20 @@
+
+/**
+ * Write a description of class Tlacidlo here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Tlacitko {
+    /**
+     * Constructor for objects of class Tlacidlo
+     */
+    private Obrazok symbol;
+    
+    public Tlacitko() {
+        this.symbol = new Obrazok("//smajlik.jpg");
+        
+    }
+    
+    
+}
