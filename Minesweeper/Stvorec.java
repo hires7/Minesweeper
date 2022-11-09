6f43bd6ff1fd1ccdc325e8dcff002ea996e73bd3
JakubHires <hires7@stud.uniza.sk>
2022-11-09 11:29:45 +0100

    a

---
+++ Minesweeper/Stvorec.java
@@ -0,0 +1,24 @@
+public class Stvorec {
+    private int poziciaX;
+    private int poziciaY;
+    private int stranaA;
+    private int obsah;
+    public Stvorec(int poziciaX, int poziciaY, int stranaA, int obsah) {
+        this.poziciaX = poziciaX;
+        this.poziciaY = poziciaY;
+        this.stranaA = stranaA;
+        this.obsah = obsah;
+    }
+    
+    public int getObsah(){
+        return this.obsah;
+    }
+    
+    public boolean getStavZobrazenia(){
+         
+    }
+    
+    public void zobraz(){
+        
+    }
+}
